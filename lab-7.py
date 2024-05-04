c3f171171d0d8eba9903b7732821ab7fbc40046c
Serge <erges699@gmail.com>
2024-05-04 19:28:33 +0300

    drawing

--- lab-7.py
+++ lab-7.py
@@ -18,3 +18,52 @@ centrality = nx.eigenvector_centrality_numpy(G)
 for n in centrality:
     print("c(", n, ")=", centrality[n])
 '''
+
+'''
+import networkx as nx
+
+#---directed graph---
+G = nx.DiGraph(directed=True)
+
+# add nodes
+G.add_node("Singapore")
+G.add_node("San Francisco")
+G.add_node("Tokyo")
+G.add_nodes_from(["Riga", "Copenhagen"])
+
+# add edges
+G.add_edge("Singapore","San Francisco")
+G.add_edge("San Francisco","Tokyo")
+G.add_edges_from(
+    [
+        ("Riga","Copenhagen"),
+        ("Copenhagen","Singapore"),
+        ("Singapore","Tokyo"),
+        ("Riga","San Francisco"),
+        ("San Francisco","Singapore"),
+    ]
+)
+# set layout
+pos = nx.circular_layout(G)
+
+# draw graph
+nx.draw(G, pos, with_labels = True)
+
+# draw edge labels
+nx.draw_networkx_edge_labels(
+    G, pos,
+    edge_labels={
+        ("Singapore","Tokyo"): '2 flights daily', 
+        ("San Francisco","Singapore"): '5 flights daily',
+    },
+    font_color='red'
+)
+'''
+import networkx as nx
+import matplotlib.pyplot as plt
+
+G = nx.petersen_graph()
+subax1 = plt.subplot(121)
+nx.draw(G, with_labels=True, font_weight='bold')
+subax2 = plt.subplot(122)
+nx.draw_shell(G, nlist=[range(5, 10), range(5)], with_labels=True, font_weight='bold')
