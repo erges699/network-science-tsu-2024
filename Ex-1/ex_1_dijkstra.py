0ae58e2286eefd6c6cf5d4f927548269403e4475
Serge Balyaba <erges699@gmail.com>
2024-05-13 17:16:28 +0300

    Ex-1-20240513-02

--- Ex-1/ex_1_dijkstra.py
+++ Ex-1/ex_1_dijkstra.py
@@ -6,6 +6,44 @@
 в составе сторонних библиотек.
 '''
 
+class Graph(object):
+    def __init__(self, nodes, init_graph):
+        self.nodes = nodes
+        self.graph = self.construct_graph(nodes, init_graph)
+
+    def construct_graph(self, nodes, init_graph):
+        '''
+        Этот метод обеспечивает симметричность графика. Другими словами, если существует путь от узла A к B со значением V, должен быть путь от узла B к узлу A со значением V.
+        '''
+        graph = {}
+        for node in nodes:
+            graph[node] = {}
+
+        graph.update(init_graph)
+
+        for node, edges in graph.items():
+            for adjacent_node, value in edges.items():
+                if graph[adjacent_node].get(node, False) == False:
+                    graph[adjacent_node][node] = value
+
+        return graph
+
+    def get_nodes(self):
+        "Возвращает узлы графа"
+        return self.nodes
+
+    def get_outgoing_edges(self, node):
+        "Возвращает соседей узла"
+        connections = []
+        for out_node in self.nodes:
+            if self.graph[node].get(out_node, False) != False:
+                connections.append(out_node)
+        return connections
+
+    def value(self, node1, node2):
+        "Возвращает значение ребра между двумя узлами."
+        return self.graph[node1][node2]
+
 #Тестовый граф из рувики https://ru.wikipedia.org/wiki/Алгоритм_Дейкстры
 init_graph = {
   1: {2:7, 3:9, 6:14},
