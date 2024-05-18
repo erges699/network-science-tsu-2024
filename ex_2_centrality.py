80838062da60d2c9a3cf805b0672df56f22ba3e1
Serge <erges699@gmail.com>
2024-05-18 22:27:31 +0300

    20240518-02

--- ex_2_centrality.py
+++ ex_2_centrality.py
@@ -12,9 +12,15 @@
 '''
 import networkx as nx
 
+
 number_of_nodes = 7
 
 G = nx.path_graph(number_of_nodes)
+G.add_edge(2, 4)
+G.add_edge(3, 1)
+G.add_edge(3, 5)
+G.add_edge(4, 5)
+G.add_edge(5, 6)
 centrality = nx.eigenvector_centrality_numpy(G)
 for n in centrality:
-    print("c(", n, ")=", centrality[n])
+    print(f'c({n}) = {centrality[n]:.4f}')
